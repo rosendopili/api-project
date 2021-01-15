b304820f2f12b1f4d537c927abe2eab1fc0aad44
Rosendo Pili <rosendopili@gmail.com>
2021-01-15 18:33:39 -0500

    api-project work

---
+++ script.js
@@ -0,0 +1,42 @@
+import {info} from './env.js'; 
+const body = document.querySelector('body'); 
+const button = document.querySelector('button'); 
+const word = document.createElement('h1'); 
+body.appendChild(word); 
+const definition = document.createElement('p'); 
+body.appendChild(definition); 
+
+const randomWord = () => {
+    fetch('https://random-word-api.herokuapp.com/word?number=1')
+    .then(response => {
+        return response.json(); 
+    })
+    .then(response => {
+        word.textContent = response
+        console.log(response);
+        randomDefinition(word);
+    })
+    .catch(err => {
+        console.log(err); 
+    }); 
+}
+
+const randomDefinition = () => {
+    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word.textContent}?key=${info.key}`)
+    .then(response => {
+        return response.json(); 
+    })
+    .then(response => {
+        console.log(response[0].shortdef[0]); 
+        definition.textContent = "Definition: " + response[0].shortdef[0]; 
+    })
+    .catch(err => {
+       console.log(err); 
+    });
+
+}
+
+button.addEventListener('click', function(){
+    randomWord(); 
+})
+
